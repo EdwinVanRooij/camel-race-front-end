a94816c440f85c66f937037f785dbfc6e77ee823
Edwin van Rooij <edwinvrooij@gmail.com>
2017-06-10 17:26:21 +0200

    functional socket sending through lobby

--- app/components/game/Game.jsx
+++ app/components/game/Game.jsx
@@ -9,61 +9,18 @@ class Game extends React.Component {
 
         this.state = {
             ws: new WebSocket('ws://192.168.5.115:8082/host'),
-            gameId: '',
-            players: [
-                {id: 9001, name: 'Henk', bidValue: '3', cardType: 'hearts'},
-                {id: 9002, name: 'Klaas', bidValue: '2', cardType: 'clubs'},
-                {id: 9003, name: 'Peter', bidValue: '4', cardType: 'diamonds'},
-                {id: 9004, name: 'Jan', bidValue: '5', cardType: 'spades'},
-            ],
-            currentScreen: 'lobby'
+            gameId: 'default',
+            currentScreen: 'lobby',
         };
         this.state.ws.onopen = () => {
             this.state.ws.send("{'eventType': 'gameCreate'}");
         };
+    }
 
-        this.state.ws.onmessage = (event) => {
-            const obj = JSON.parse(event.data);
-            switch (obj.eventType) {
-                case 'gameCreated':
-                    this.setState({
-                        gameId: obj.value.id
-                    });
-                    break;
-                case 'playerJoined':
-                    let newPlayer = {id: obj.value.id, name: obj.value.name};
-                    // let newPlayer = {id: obj.value.id, name: obj.value.name, bidValue: '-', cardType: '-'};
-
-                    this.state.players.push(newPlayer);
-                    this.setState({
-                        players: this.state.players
-                    });
-                    break;
-                case 'playerNewBid':
-                    let newPlayer2 = {
-                        id: obj.value.player.id,
-                        name: obj.value.player.name,
-                        bidValue: obj.value.bid.value,
-                        cardType: obj.value.bid.type
-                    };
-
-                    const newPlayers = [];
-
-                    this.state.players.forEach(function (item, i) {
-                        if (item.id === newPlayer2.id) {
-                            newPlayers.push(newPlayer2);
-                        } else {
-                            newPlayers.push(item);
-                        }
-                    });
-                    this.setState({
-                        players: newPlayers
-                    });
-                    break;
-                default:
-                    alert('Could not determine eventType \'' + obj.eventType + '\'')
-            }
-        };
+    handleGameIdReceived(id) {
+        this.setState({
+            gameId: id
+        });
     }
 
     handleOnStartClick() {
@@ -75,8 +32,7 @@ class Game extends React.Component {
             case 'lobby': {
                 return (
                     <div>
-                        <Lobby onStartClick={() => this.handleOnStartClick()} players={this.state.players}
-                               gameId={this.state.gameId}/>
+                        <Lobby onGameIdReceived={(val) => this.handleGameIdReceived(val)} onStartClick={() => this.handleOnStartClick()} ws={this.state.ws} gameId={this.state.gameId}/>
                     </div>
                 );
             }
@@ -90,14 +46,12 @@ class Game extends React.Component {
             case 'results': {
                 return (
                     <div>
-                        <Lobby onStartClick={() => this.handleOnStartClick()} players={this.state.players}
-                               gameId={this.state.gameId}/>
+                        <Lobby onStartClick={() => this.handleOnStartClick()} gameId={this.state.gameId}/>
                     </div>
                 );
             }
             default: {
-                //statements;
-                alert('Could not find out which screen to be at, it\'s ' + this.state.currentScreen + ' rn');
+                console.log('Could not find out which screen to be at, it\'s ' + this.state.currentScreen + ' rn');
                 break;
             }
         }
--- app/components/game/lobby/Lobby.jsx
+++ app/components/game/lobby/Lobby.jsx
@@ -3,22 +3,78 @@ import PageHeader from 'PageHeader';
 import LobbyConnectedBox from 'LobbyConnectedBox';
 import LobbyStartBox from 'LobbyStartBox';
 
-function Lobby(props) {
-    return (
-        <div>
+class Lobby extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ws: props.ws,
+            currentScreen: 'lobby',
+            players: [],
+        };
+
+        this.state.ws.onmessage = (event) => {
+            console.log('Message from lobby');
+            const obj = JSON.parse(event.data);
+            switch (obj.eventType) {
+                case 'gameCreated':
+                    const gameId = obj.value.id;
+                    this.props.onGameIdReceived(gameId);
+                    break;
+                case 'playerJoined':
+                    let newPlayer = {id: obj.value.id, name: obj.value.name};
+                    // let newPlayer = {id: obj.value.id, name: obj.value.name, bidValue: '-', cardType: '-'};
+
+                    this.state.players.push(newPlayer);
+                    this.setState({
+                        players: this.state.players
+                    });
+                    break;
+                case 'playerNewBid':
+                    let newPlayer2 = {
+                        id: obj.value.player.id,
+                        name: obj.value.player.name,
+                        bidValue: obj.value.bid.value,
+                        cardType: obj.value.bid.type
+                    };
+
+                    const newPlayers = [];
+
+                    this.state.players.forEach(function (item, i) {
+                        if (item.id === newPlayer2.id) {
+                            newPlayers.push(newPlayer2);
+                        } else {
+                            newPlayers.push(item);
+                        }
+                    });
+                    this.setState({
+                        players: newPlayers
+                    });
+                    break;
+                default:
+                    console.log('Could not determine eventType \'' + obj.eventType + '\'')
+            }
+        };
+    }
+
+    render () {
+        return (
             <div>
-                <PageHeader title={'Lobby!'}/>
-            </div>
-            <div className="row container">
-                <div className="columns small-4 medium-4 large-4 connected-box">
-                    <LobbyConnectedBox players={props.players}/>
+                <div>
+                    <PageHeader title={'Lobby!'}/>
                 </div>
-                <div className="columns small-8 medium-8 large-8 start-box">
-                    <LobbyStartBox onStartClick={props.onStartClick} gameId={props.gameId}/>
+                <div className="row container">
+                    <div className="columns small-4 medium-4 large-4 connected-box">
+                        <LobbyConnectedBox players={this.state.players}/>
+                    </div>
+                    <div className="columns small-8 medium-8 large-8 start-box">
+                        <LobbyStartBox onStartClick={this.props.onStartClick} gameId={this.props.gameId}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 module.exports = Lobby;
