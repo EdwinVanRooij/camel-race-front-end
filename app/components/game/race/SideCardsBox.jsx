e06d4b2d93e3c83e64710ebea85edb651a8eb13b
Edwin <edwinvrooij@gmail.com>
2017-06-14 11:42:47 +0200

    pre animations

--- app/components/game/race/SideCardsBox.jsx
+++ app/components/game/race/SideCardsBox.jsx
@@ -6,11 +6,16 @@ import cardToUrl from 'Util';
 function SideCard(props) {
     if (props.sideCard.isVisible === true) {
         return (
-            <div className="side-card">
-                <img src={cardToUrl.cardToUrl(props.sideCard.cardType,props.sideCard.cardValue)}/>
+            <div className="flip-container this.classList.toggle('hover')">
+                <div className="side-card front">
+                    <img src={CardUnknown}/>
+                </div>
+                <div className="side-card back">
+                    <img src={cardToUrl.cardToUrl(props.sideCard.cardType, props.sideCard.cardValue)}/>
+                </div>
             </div>
         )
-    } else if (props.sideCard.isVisible === false){
+    } else if (props.sideCard.isVisible === false) {
         return (
             <div className="side-card">
                 <img src={CardUnknown}/>
