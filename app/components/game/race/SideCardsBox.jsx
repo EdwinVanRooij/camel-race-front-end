4160583c44370fdee94e6c9f214cdc31bcc5b984
Edwin <edwinvrooij@gmail.com>
2017-06-15 09:09:11 +0200

    fixed sidecard bug

--- app/components/game/race/SideCardsBox.jsx
+++ app/components/game/race/SideCardsBox.jsx
@@ -6,13 +6,8 @@ import cardToUrl from 'Util';
 function SideCard(props) {
     if (props.sideCard.isVisible === true) {
         return (
-            <div className="flip-container this.classList.toggle('hover')">
-                <div className="side-card front">
-                    <img src={CardUnknown}/>
-                </div>
-                <div className="side-card back">
-                    <img src={cardToUrl.cardToUrl(props.sideCard.cardType, props.sideCard.cardValue)}/>
-                </div>
+            <div className="side-card">
+                <img src={cardToUrl.cardToUrl(props.sideCard.cardType, props.sideCard.cardValue)}/>
             </div>
         )
     } else if (props.sideCard.isVisible === false) {
